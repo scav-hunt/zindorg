90efe26a8cb06c2a1e9afd390a949482a9b83d83
Gabi Garcia <gabigarcia@gabis-mbp.lan>
2024-04-21 22:20:25 -0500

    having a bad time here in passwordland

--- assets/js/password-handler.js
+++ assets/js/password-handler.js
@@ -3,22 +3,40 @@
  * exact correct password is entered (javascript corrects to allcaps
  * and no whitespace)
  */
-window.onload = function () {
-    let pwdInput = document.getElementById("passwordInput");
 
-    pwdInput.addEventListener('input change keyup', inputChanged());
-
-    window.onkeyup = inputChanged();
-
-    function inputChanged() {
+    function submitPromoCode(passwords) {
+        console.log(passwords);
+        //format is ["password": PWD, "slug": SLUG]
+        let pwdInput = document.getElementById("passwordInput");
         let input = pwdInput.value;
         console.log(input);
-
-        if (input != "") {
-            let clean = input.toUpperCase();
-            clean.replaceAll(' ','');
-            console.log(clean);
+        let clean = input.toUpperCase();
+        console.log(clean);
+        clean = clean.replace(/\s/g, '');
+        console.log(clean);
+        for (pwd in passwords) {
+            if (clean == pwd.password) {
+                console.log("match!");
+            }
         }
-        //take input and internally capitalize and remove spaces
     }
-}
+
+// window.onload = function () {
+//     let pwdInput = document.getElementById("passwordInput");
+
+//     pwdInput.addEventListener('input change keyup', inputChanged());
+
+//     window.onkeyup = inputChanged();
+
+//     function inputChanged() {
+//         let input = pwdInput.value;
+//         console.log(input);
+
+//         if (input != "") {
+//             let clean = input.toUpperCase();
+//             clean.replaceAll(' ','');
+//             console.log(clean);
+//         }
+//         //take input and internally capitalize and remove spaces
+//     }
+// }
